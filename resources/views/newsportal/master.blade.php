b1ee9eb512cd52f22d76a9db494e1217bf5ffdb3
worldshero <bhartimukund49@gmail.com>
2019-03-06 10:14:20 +0530

    Login_register

--- resources/views/newsportal/master.blade.php
+++ resources/views/newsportal/master.blade.php
@@ -1,3 +1,6 @@
+
+
+
 <!DOCTYPE html>
 <html lang="en">
             <head>
@@ -5,67 +8,10 @@
               <meta name="viewport" content="width=device-width, initial-scale=1.0">
               <meta http-equiv="X-UA-Compatible" content="ie=edge">
               <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+              <link rel="stylesheet" href="{{ asset('/css/castom.css') }}">
+              <link rel="stylesheet" href="{{ asset('/css/login.css') }}">
               {{-- CSS for Dropdown table  --}}
-                  <style>
-                        .dropbtn {
-                          background-color: #66a3ff;
-                          color: white;
-                          padding: 10px;
-                          font-size: 16px;
-                          border: none;}
-                         .dropdown {
-                            position: relative;
-                            display: inline-block;}
-                          .dropdown-content {
-                            display: none;
-                            position: absolute;
-                            background-color: #f1f1f1;
-                            min-width: 160px;
-                            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-                            z-index: 1;}
-                          .dropdown-content a {
-                            color: black;
-                            padding: 12px 16px;
-                            text-decoration: none;
-                            display: block;}
-                          .dropdown-content a:hover {background-color: #ddd;}
-                          .dropdown:hover .dropdown-content {display: block;}
-                          .dropdown:hover .dropbtn {background-color:  #66a3ff;}
 
-                            /* end CSS for Dropdown table nav button */
-                            /* navbar background font size */
-                          .bgc {
-                             background-color: #e6f0ff;
-                             color: white;
-                             padding: 5px;
-                             font-size: 10px;
-                             border: none;
-                             text-align:; }
-                          a:hover { background-color:  #66a3ff;}
-                           /* social logo style */
-                          .scs {
-                             background-color: #66a3ff;
-                              color: white;
-                              padding: 8px;
-                              font-size: 16px;
-                              border: none;
-                              text-align:;}
-                           /* for overflow text */
-                         .text {
-                            display: block;
-                            height: 50px;
-                            margin: 0 auto;
-                            line-height: 1.2;
-                            -webkit-line-clamp: 3;
-                            -webkit-box-orient: vertical;
-                            overflow: hidden;
-                             text-overflow: ellipsis;}
-                           /* image slider in inedex page */
-                           /* Make the image fully responsive */
-                          .carousel-inner img {
-                            width: 100%;
-                            height: 50%;}
-                       </style>
                       </head>
                          <body>
                            {{-- for image slider --}}
@@ -82,5 +28,6 @@
                             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
              <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
              <script src="{{ asset('/js/costom.js') }}"></script>
+             <script src="{{ asset('/js/login.js') }}"></script>
             </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,16 @@ Route::resource('course/detailvalue', 'CoursedetailController');
 Route::resource('course','CourseController');
 Route::resource('frontend','FrontendController');
 
+Route::resource('frontend/admin/home','AdminController');
 
 
 
 
-Route::resource('slides', 'SliderController');
 
 
 
-// Auth::routes();
 
-// Route::get('/home', 'HomeController@index')->name('home');
+
 
 
 Auth::routes();
